335ac553cc63a73b7338f85fe6fac7b7a16a61d3
Jakub Andrzejewski <jakubandrz1@gmail.com>
2021-05-20 19:23:56 +0200

    every Fn tests added

--- src/__test__/module1/task2/everyFn.test.ts
+++ src/__test__/module1/task2/everyFn.test.ts
@@ -6,4 +6,17 @@ describe('EveryFn tests', () => {
       expect(() => everyFn([], (val) => val > 0)).toThrowError('Array is empty');
     });
   });
+
+  describe('When valid arguments are provided', () => {
+    it('Should return true, because every value fulfills the condition', () => {
+      const result: boolean = everyFn([2, 4, 6, 8, 10], (val) => val % 2 === 0);
+
+      expect(result).toBeTruthy();
+    });
+
+    it('Should return false, because one value reject the condition', () => {
+      const result: boolean = everyFn([1, 2, 3, 4], (val) => val > 1);
+      expect(result).toBeFalsy();
+    });
+  });
 });
