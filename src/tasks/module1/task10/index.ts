afa9fc7fbe76fc885479ff39e28fe12e8ed2400c
Jakub Andrzejewski <jakubandrz1@gmail.com>
2021-05-21 18:07:14 +0200

    added new Error

--- src/tasks/module1/task10/index.ts
+++ src/tasks/module1/task10/index.ts
@@ -1,22 +1,25 @@
 const paginateArray = <T>(dataEntries: T[], pageIndex: number, elementsOnPage: number): T[] => {
   const length: number = dataEntries.length;
+  if (length < 1) {
+    throw new Error('Array cannot be empty');
+  }
   if (Number.isNaN(pageIndex) || Number.isNaN(elementsOnPage)) {
-    throw new Error("Number of pageIndex or elementsOnPage cannot be NaN");
+    throw new Error('Number of pageIndex or elementsOnPage cannot be NaN');
   }
   if (dataEntries.lastIndexOf(dataEntries[length - 1]) < pageIndex) {
-    throw new Error("Index to pagination is greater than the indexes of the array");
+    throw new Error('Index to pagination is greater than the indexes of the array');
   }
   if (length < elementsOnPage) {
-    throw new Error("Elements in array are shorter than the elements needed");
+    throw new Error('Elements in array are shorter than the elements needed');
   }
   if (pageIndex * elementsOnPage >= dataEntries.length) {
-    throw new Error("The array does not contain enough elements for pagination");
-  }
-  if (length < 1) {
-    throw new Error("Array cannot be empty");
+    throw new Error('The array does not contain enough elements for pagination');
   }
   if (pageIndex < 0) {
-    throw new Error("Pagination index starts with 0");
+    throw new Error('Pagination index starts with 0');
+  }
+  if (elementsOnPage <= 0) {
+    throw new Error('ElementsOnPage must be greater than 0');
   }
   const copied: T[] = [...dataEntries];
   const start: number = pageIndex * elementsOnPage;
