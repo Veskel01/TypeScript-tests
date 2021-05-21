2a3c14ad644187fd5b8be77cf9208c0c7beed370
Jakub Andrzejewski <jakubandrz1@gmail.com>
2021-05-21 17:35:48 +0200

    getMyAge change

--- src/tasks/module1/task7/index.ts
+++ src/tasks/module1/task7/index.ts
@@ -7,29 +7,29 @@ function getMyAge(input: AllowedTypes): number {
   } else if (compareInput.getFullYear() <= 1880) {
     throw new Error("You can't be that old");
   } else if (compareInput.getFullYear() === new Date().getFullYear()) {
-    throw new Error("Were you born this year?");
+    throw new Error('Were you born this year?');
   } else if (Number.isNaN(compareInput.valueOf())) {
-    throw new Error("Input cannot be NaN");
+    throw new Error('Input cannot be NaN');
   }
-  if (typeof input === "string") {
+  if (typeof input === 'string') {
     const birthDate: Date = new Date(input);
     const today: number = Date.now() - birthDate.getTime();
     const ageDate: Date = new Date(today);
     const result: number = Math.abs(ageDate.getFullYear() - 1970);
     return result;
-  } else if (typeof input === "number") {
+  } else if (typeof input === 'number') {
     const ageMS: number = Date.parse(Date()) - Date.parse(input.toString());
     const age: Date = new Date();
     age.setTime(ageMS);
     const result: number = age.getFullYear() - 1970;
     return result;
-  } else if (Object.prototype.toString.call(input) === "[object Date]") {
-    const today: number = Date.now() - input.getTime();
-    const ageDate: Date = new Date(today);
-    const result: number = Math.abs(ageDate.getFullYear() - 1970);
-    return result;
+  } else if (Object.prototype.toString.call(input) === '[object Date]') {
+    const now: Date = new Date();
+    const currentYear: number = now.getFullYear();
+    const birthday: Date = new Date(currentYear, input.getMonth(), input.getDate());
+    return birthday.getFullYear() - input.getFullYear();
   } else {
-    throw new Error("Invalid date");
+    throw new Error('Invalid date');
   }
 }
 
